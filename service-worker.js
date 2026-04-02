5eb3bb2fc62a918a250ed5ffefaf58aa11b399e9
app-WEYNII <lucadjouadii@gmail.com>
2026-04-02 09:57:25 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // ═══════════════════════════════════════════════════════
 
 // Nom du cache - changer la version force un rechargement complet
-var CACHE_NAME = 'pointeuse-v3';
+var CACHE_NAME = 'pointeuse-v4';
 
 // Fichiers essentiels à mettre en cache pour le fonctionnement offline
 var URLS_TO_CACHE = [
